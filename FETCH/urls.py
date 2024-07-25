7a3ba0751f751ddf4fb30206752df17c6d19e6c9
bhaveshkorane <bhavesh.korane@tekditechnologies.com>
2024-07-25 18:09:46 +0530

    urls.py

--- FETCH/urls.py
+++ FETCH/urls.py
@@ -19,14 +19,24 @@ from .views import DistrictGeneric
 
 
 urlpatterns = [
+
+    # For inserting data into database 
+
     path('createstate/',createstate,name='createstate_url'),
     path('createdistrict/',createdistrict,name='createdistrict_url'),
     path('createsubdistrict/',createsubdistrict,name='createsubdistrict_url'),
     path('createvillage/',createvillage,name='createvillage_url'),
     path('showstate/',showstate,name='showstate_url'),
-    path('showdistrict/',showdistrict,name='showdistrict_url'),
-    path('showsubdistrict/',showdistrict,name='showsubdistrict_url'),
-    path('showvillage/',showvillage,name='showvillage_url'),
+
+    # Delete if there is no use of below and delete form views.py also
+
+    # path('showdistrict/',showdistrict,name='showdistrict_url'),
+    # path('showsubdistrict/',showdistrict,name='showsubdistrict_url'),
+    # path('showvillage/',showvillage,name='showvillage_url'),
+
+    
+    # For fetching data from the database through api 
+    
     path('generic-village/',VillageGeneric.as_view()),
     path('generic-state/',StateGeneric.as_view()),
     path('generic-distirct/',DistrictGeneric.as_view()),
